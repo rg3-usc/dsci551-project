c412cbc16ac8a0bbedc42075ee5108da26aaf09e
Richard <rg45303@usc.edu>
2023-11-12 23:27:27 -0800

    feat: CLI for query (project, filter, group/aggregate, order); join operation WIP

--- src/json_cli.py
+++ src/json_cli.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 
 class KeyValueStore:
     PRIMARY_KEY_LOCATION = "_primary_key"
@@ -147,9 +148,108 @@ class KeyValueStore:
     def close(self):
         pass
 
+def load_json_data(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
 def main():
     file_path = input("Enter the path to the database file: ")
     database = KeyValueStore(file_path)
+        
+    def project(data, fields):
+        return [{field: item.get(field) for field in fields} for item in data]
+
+    def filter_data(data, query):
+        match = re.match(r'(\w+)\s*([=><!]+)\s*(\'[^\']*\'|"[^"]*"|\w+)', query)
+        if not match:
+            match = re.match(r'(\w+)\s+contains\s+(\'[^\']*\'|"[^"]*"|\w+)', query)
+            if not match:
+                raise ValueError(f'Invalid query: {query}')
+            field, value = match.groups()
+            # Convert both the field value and the search value to lowercase for case-insensitive matching
+            condition = f"{value.lower()} in item.get('{field}').lower()"
+        else:
+            field, op, value = match.groups()
+            if op == '=': op = '=='
+            condition = f"item.get('{field}') {op} {value}"
+        filtered_data = [item for item in data if eval(condition, {"item": item})]
+        return filtered_data
+
+    '''def custom_merge(data, key, other_data):
+        merged_data = []
+        key_index = {item[key]: item for item in other_data}
+        for item in data:
+            key_value = item.get(key)
+            if key_value in key_index:
+                item.update(key_index[key_value])
+                merged_data.append(item)
+        return merged_data'''
+    
+    def count(data, group_by=None):
+        results = []
+        if group_by is not None:
+            def group(data, field):
+                grouped_data = {}
+                for item in data:
+                    key = item[field]
+                    grouped_data.setdefault(key, []).append(item)
+                return grouped_data
+            grouped_data = group(data, group_by)
+            for key, group in grouped_data.items():
+                count_result = len(group)
+                results.append({group_by: key, "count": count_result})
+        else:
+            count_result = len(data)
+            results.append({"count": count_result})
+        return results
+    def find(data, aggregation, field, group_by=None):
+        results = []
+        if group_by is not None:
+            def group(data, field):
+                grouped_data = {}
+                for item in data:
+                    key = item[field]
+                    grouped_data.setdefault(key, []).append(item)
+                return grouped_data
+            grouped_data = group(data, group_by)
+            for key, group in grouped_data.items():
+                if aggregation == "sum":
+                    agg_result = sum(item.get(field, 0) for item in group)
+                elif aggregation == "min":
+                    agg_result = min((item.get(field, None) for item in group if field in item), default=None)
+                elif aggregation == "max":
+                    agg_result = max((item.get(field, None) for item in group if field in item), default=None)
+                elif aggregation == "average":
+                    valid_items = [item.get(field) for item in group if field in item]
+                    agg_result = sum(valid_items) / len(valid_items) if valid_items else None
+                elif aggregation == "count":
+                    agg_result = sum(1 for item in group)
+                results.append({group_by: key, field: agg_result, "aggregation": aggregation})
+        else:
+            if aggregation == "sum":
+                result = sum(item.get(field, 0) for item in data)
+            elif aggregation == "min":
+                result = min((item.get(field, None) for item in data if field in item), default=None)
+            elif aggregation == "max":
+                result = max((item.get(field, None) for item in data if field in item), default=None)
+            elif aggregation == "average":
+                valid_items = [item.get(field) for item in data if field in item]
+                result = sum(valid_items) / len(valid_items) if valid_items else None
+            elif aggregation == "count":
+                result = sum(1 for item in data)
+            results.append({field: result, "aggregation": aggregation})
+        return results
+
+    def sort(data, fields):
+        sorted_data = data
+        for field in reversed(fields):
+            if field.startswith('-'):
+                field = field[1:]  # Remove the '-' for sorting
+                sorted_data = sorted(sorted_data, key=lambda item: item.get(field), reverse=True)
+            else:
+                sorted_data = sorted(sorted_data, key=lambda item: item.get(field))
+        return sorted_data
 
     # Check if the primary key is already set
     if not database.primary_key:
@@ -166,7 +266,8 @@ def main():
         print("3. Update data (JSON format)")
         print("4. Delete data")
         print("5. Display data")
-        print("6. Exit")
+        print("6. Query data")
+        print("7. Exit")
         choice = input("\nEnter your choice: ")
 
         if choice == '1':
@@ -208,6 +309,37 @@ def main():
                 print(item)
 
         elif choice == '6':
+            query = input("Enter your custom query (e.g., 'project name stars | filter stars > 4'): ")
+            queries = query.strip().split('|')
+            data = database.read_data()
+            for query in queries:
+                parts = query.strip().split()
+                operation = parts[0]
+                if operation == 'project':
+                    fields = parts[1:]
+                    data = project(data, fields)
+                elif operation == 'filter':
+                    condition = ' '.join(parts[1:])
+                    data = filter_data(data, condition)
+                elif operation == 'join':
+                    pass
+                elif operation == 'find':
+                    aggregation = parts[1]
+                    if aggregation == "count":
+                        group_by = parts[3] if len(parts) > 2 and parts[2] == 'by' else None
+                        data = count(data, group_by)
+                    else:
+                        field = parts[2]
+                        group_by = parts[4] if len(parts) > 4 and parts[3] == 'by' else None
+                        data = find(data, aggregation, field, group_by)
+                elif operation == 'order':
+                    fields = parts[1:]
+                    data = sort(data, fields)
+            print("\nQuery result:")
+            for item in data:
+                print(item)
+
+        elif choice == '7':
             print("\nExiting the program.")
             database.close()
             break
